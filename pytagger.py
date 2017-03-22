8e8148a25dd3054ef18979f7c55413df30d7a9a5
jasper <jasper@vanderstoop.nl>
2017-03-22 16:49:31 +0100

    Added optional arguments. Added Topic Lexicon.

--- pytagger.py
+++ pytagger.py
@@ -1,10 +1,17 @@
 from .modules import init_config
 from .modules import tag
 
-def doTag(text=False):
+def doTag(text=False, lexicon=False, tags=False):
     config = init_config()
 
     if text:
         config.text = text
+
+    if lexicon:
+        config.lexicon = lexicon
+
+    if tags:
+        if isinstance(tags, list):
+            config.tags = tags
 
     return tag(config)
